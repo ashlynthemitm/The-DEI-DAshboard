48362912a90ceff2c9ac66f7784041b24b43ba15
ashlynthemitm <ashlyncmitm@gmail.com>
2023-02-04 16:14:09 -0500

    Initial GUI Changes and clean up

--- DashGUI.java
+++ DashGUI.java
@@ -17,16 +17,17 @@ import java.awt.event.ComponentListener;
 // Other companies may include sign on bonus, stocks etc.
 // <first_name,last_name,gender,age,sexuality,religion,experience_level,position,political_office,duration_of_employment,annual_salary,hourly_salary,benefit_plan>
 public class DashGUI extends JFrame implements ActionListener, ComponentListener  {
-    final int button_WIDTH = 20;
-    final int button_HEIGHT = 25;
-    final int frame_WIDTH = 500;
-    final int frame_HEIGHT = 500;
+    final int buttonWIDTH = 20;
+    final int buttonHEIGHT = 25;
+    final int frameWIDTH = 500;
+    final int frameHEIGHT = 500;
 
-    static JTextField username;
-    static JTextField passwrd;
-    static JButton login_button;
-    static JPanel panel;
+    static JTextField usernameText;
+    static JPasswordField passwordText;
+    static JButton loginButton;
+    static JPanel mainPanel;
     static JFrame dashboard;
+
 
     public JFrame dashboard() { // dashboard design
         dashboard = new JFrame();
@@ -34,54 +35,31 @@ public class DashGUI extends JFrame implements ActionListener, ComponentListener
         dashboard.setLocation(new Point(400,100));
         dashboard.setLayout(new BorderLayout());
         dashboard.setTitle("The DAshBoard");
-        dashboard.setSize(frame_WIDTH, frame_HEIGHT);
+        dashboard.setSize(frameWIDTH, frameHEIGHT);
 
-        // Creating the login panel
-        panel = new JPanel(null);
-        panel.setBackground(Color.pink);
-        panel.setOpaque(true); panel.setPreferredSize(getPreferredSize());
-        JLabel login_label = new JLabel("Login to Admin Account"); login_label = FontSize(login_label);
-        JLabel user_name = new JLabel("Username");
-        JLabel pw_name = new JLabel("Password");
-        username = new JTextField("Username");
-        passwrd = new JTextField("Password"); // This text field will be shown twice, to reenter password
-        login_button = new JButton("Login");
-        JButton view_reports = new JButton("View Reports");
-
-
-
-        // TextField Layouts
-
-        username = TextLayout(username);
-        passwrd = TextLayout(passwrd);
-        user_name.setBounds(150, 48, 70, 20);
-        panel.add(user_name);
-        username.setBounds(150, 67, 193, 28);
-        panel.add(username);
-        pw_name.setBounds(150, 100, 70, 20);
-        panel.add(pw_name);
-        passwrd.setBounds(150, 125, 193, 28);
-        panel.add(passwrd);
-
-        login_button.setBounds(193, 160, 90, 25);
-        login_button.setForeground(Color.WHITE);
-        login_button.setBackground(Color.BLACK);
-        panel.add(login_button);
-
-        view_reports.setBounds(180, 190, 120, 25);
-        view_reports.setForeground(Color.WHITE);
-        view_reports.setBackground(Color.BLACK);
-        panel.add(view_reports);
-
-        dashboard.add(panel, BorderLayout.CENTER);
-
-
-        login_button.addActionListener(this);
-        view_reports.addActionListener(this);
+        // general size and color of the login panel // now being created in its own class
+        mainPanel = new JPanel(null);
+        mainPanel.setBackground(Color.BLACK);
+        mainPanel.setOpaque(true); mainPanel.setPreferredSize(getPreferredSize());
+        JLabel loginLabel = new JLabel("Login to Admin Account"); loginLabel = FontSize(loginLabel);
+        JLabel usernameLabel = new JLabel("Username");
+        JLabel passwordLabel = new JLabel("Password");
+        usernameText = new JTextField(20);
+        passwordText = new JPasswordField(); // This text field will be shown twice, to reenter password
+        loginButton = new JButton("Login");
+        JButton viewReports = new JButton("View Reports");
 
 
 
-//<first_name,last_name,gender,age,sexuality,religion,experience_level,position,political_office,duration_of_employment,annual_salary,hourly_salary,benefit_plan>
+        // Login
+        mainPanel = login_acct(usernameText, passwordText, usernameLabel,passwordLabel,viewReports);
+        dashboard.add(mainPanel, BorderLayout.CENTER);
+
+
+        loginButton.addActionListener(this);
+        viewReports.addActionListener(this);
+
+
 
 
 
@@ -91,11 +69,11 @@ public class DashGUI extends JFrame implements ActionListener, ComponentListener
         return dashboard;
     }
     public JButton ButtonLayout(JButton bt) {
-        bt.setPreferredSize(new Dimension(button_WIDTH, button_HEIGHT));
+        bt.setPreferredSize(new Dimension(buttonWIDTH, buttonHEIGHT));
         bt.setForeground(Color.gray);
         return bt;
     }public JTextField TextLayout(JTextField tf) {
-        tf.setPreferredSize(new Dimension(button_WIDTH, button_HEIGHT));
+        tf.setPreferredSize(new Dimension(buttonWIDTH, buttonHEIGHT));
         tf.setForeground(Color.gray);
         return tf;
     } public JLabel FontSize(JLabel jl) {
@@ -108,33 +86,64 @@ public class DashGUI extends JFrame implements ActionListener, ComponentListener
     public void actionPerformed(ActionEvent e) { // My action listener to add to my panel
 
 
-        if ( e.getActionCommand().equals("Login")) {
-            String Username = username.getText();
-            String Password = passwrd.getText();
+        if ( e.getActionCommand().equals("Login")) { // once the login button is selected
             account_info checkAcct = new account_info();
 
-            int id = checkAcct.check_admin(Username, Password);
+            int id = checkAcct.check_admin(usernameText.getText(),passwordText.getText());
             id = 4;
             if ( id!=0) {
                 JOptionPane.showMessageDialog(null, "Login Successful");
                 // change the panel to admin design
-                //panel = admin_acct();
-                JPanel form = admin_acct(); // Get the screen to change
-                dashboard.add(form);
+                dashboard.removeAll();
+                mainPanel = admin_acct();
+
+//                JPanel form = admin_acct();
+//                panel.add(form);
+//                dashboard.add(form);
 
             }
             else
                 JOptionPane.showMessageDialog(null, "Username or Password is incorrect");
 
-        } else {
-            // else its the view reports button
-            // the panel will be updated
+        } else { // Else the view reports button is selected
             myJDBC db = new myJDBC();
+
 
 
 
         }
     }
+
+    public static JPanel login_acct(JTextField usernameText, JPasswordField passwordText, JLabel usernameLabel, JLabel passwordLabel,JButton viewReports){
+        JPanel loginPanel = new JPanel();
+        loginPanel = new JPanel(null);
+        loginPanel.setBackground(Color.pink);
+        loginPanel.setOpaque(true);
+        usernameLabel.setBounds(150, 48, 70, 20);
+        loginPanel.add(usernameLabel);
+        usernameText.setBounds(150, 67, 193, 28);
+        loginPanel.add(usernameText);
+        passwordLabel.setBounds(150, 100, 70, 20);
+        loginPanel.add(passwordLabel);
+        passwordText.setBounds(150, 125, 193, 28);
+        loginPanel.add(passwordText);
+
+        loginButton.setBounds(193, 160, 90, 25);
+        loginButton.setForeground(Color.WHITE);
+        loginButton.setBackground(Color.BLACK);
+        loginPanel.add(loginButton);
+
+        viewReports.setBounds(180, 190, 120, 25);
+        viewReports.setForeground(Color.WHITE);
+        viewReports.setBackground(Color.BLACK);
+        loginPanel.add(viewReports);
+
+        return loginPanel;
+
+    }
+
+
+
 
     public static JPanel admin_acct(){ // designing what the admin account dashboard looks like looks like
         JPanel form = new JPanel();
--- myJDBC.java
+++ myJDBC.java
@@ -1,43 +1,53 @@
 import java.sql.*;
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class myJDBC {
 
 
     public static String check_database(String string) throws SQLException { // Anyone can view reports as an guest it is simplified to
-                // but it'll be sent in as the action --> view, insert (objective)
-                if(string!="view")
-                    string = "insert into staff" +
-                                "(first_name,last_name,gender,age,sexuality,religion,experience_level,position,political_office,duration_of_employment,annual_salary,hourly_salary,benefit_plan)" +
-                                "VALUES\n" +
-                                "("+string+")";
-                else
-                    string = "select * from staff";
+        // but it'll be sent in as the action --> view, insert (objective)
+        // This file view and grabs information from the database
+        if (string != "view") {
+            string = "insert into staff" +
+                    "(first_name,last_name,gender,age,sexuality,religion,experience_level,position,political_office,duration_of_employment,annual_salary,hourly_salary,benefit_plan)" +
+                    "VALUES\n" +
+                    "(" + string + ")";
+        }
+        else
+            string = "select * from staff";
 
-                //<first_name,last_name,gender,age,sexuality,religion,experience_level,position,political_office,duration_of_employment,annual_salary,hourly_salary,benefit_plan>
-                try {
-                    Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc-wpgui", "root", "Harlem3140!");
+        //<first_name,last_name,gender,age,sexuality,religion,experience_level,position,political_office,duration_of_employment,annual_salary,hourly_salary,benefit_plan>
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc-wpgui", "root", "Harlem3140!");
 
-                    Statement statement = connection.createStatement();
+            Statement statement = connection.createStatement();
 
-                    // results returned from the database
-                    ResultSet result = statement.executeQuery(string); // hopefully it works correctly
+            // results returned from the database
+            ResultSet result = statement.executeQuery(string);
 
-                    switch (string) { // the string will change to the correct return value within this switch statement
-                    case "view":
+            switch (string) { // the string will change to the correct return value within this switch statement
+                case "view":
                     while (result.next()) {
-                        string += (result.getString("gender") +", "+ result.getString("age")+", "+result.getString("sexuality") +", "+ result.getString("religion")+", "+result.getString("experience_level") +", "+ result.getString("political_office")+", "+result.getString("position") +", "+result.getString("duration_of_employment")+", "+result.getString("annual_salary")+", "+result.getString("hourly_salary")+", "+result.getString("benefit_plan"));
+                        string += (result.getString("gender") + ", " + result.getString("age") + ", " + result.getString("sexuality") + ", " + result.getString("religion") + ", " + result.getString("experience_level") + ", " + result.getString("political_office") + ", " + result.getString("position") + ", " + result.getString("duration_of_employment") + ", " + result.getString("annual_salary") + ", " + result.getString("hourly_salary") + ", " + result.getString("benefit_plan"));
                         string += "\n";
                     }
+                default:
+                    // default
 
+            }
+            connection.close();
 
-                    }
-                } catch (Exception e){
-                    e.printStackTrace();
-                }
-                return string;
-         // Finished with View Reports
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return string;
+
+        // Finished with View Reports
 
     }
-
 
 }
